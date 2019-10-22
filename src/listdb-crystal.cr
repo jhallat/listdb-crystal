32608765ccda6b5228d736f53206bf1798125af4
John Hallat <jhallat@gmail.com>
2019-10-21 22:20:53 -0400

    added create command

--- src/listdb-crystal.cr
+++ src/listdb-crystal.cr
@@ -1,15 +1,33 @@
 # TODO: Write documentation for `Listdb::Crystal`
+def create(args) 
+  if args.size > 0
+    puts args
+  else 
+    puts "I don't know what you want me to create."  
+  end   
+end
+
 module Listdb::Crystal
   VERSION = "0.1.0"
 
   loop do 
     print "> "
-    command = gets
-    if command
-      if command.strip.upcase == "EXIT"
-        break
+    input = gets
+    if input 
+      args = input.split(' ')
+      if args.size > 0
+        command = args.shift
+        case command.strip.upcase
+        when "EXIT"
+            break
+        when "CREATE"
+          create(args)  
+        else
+          puts "I don't know what you are asking me to do."
+        end
       end
     end
   end
   
+
 end
